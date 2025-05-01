267e431b829f188c13310b6b5d4bde47da237b75
Viviana Enriquez <176007837+DoomMetalLady@users.noreply.github.com>
2025-05-01 16:14:55 -0300

    hice clases hijas moto y auto

--- ejercicio.py
+++ ejercicio.py
@@ -5,3 +5,15 @@ class Vehículo():
         self.color = color
     def mostrar_info(self):
         return f"Vehículo -- {self.modelo} -- {self.marca} -- {self.color}"
+
+class Moto(Vehiculo):
+    def__init__(self,modelo,marca,color,ruedas,luces):
+        super().__init__(modelo,marca,color)
+        self.ruedas = ruedas
+        self.luces = luces
+
+class Auto(Vehiculo):
+    def__init__(self,modelo,marca,color,ruedas,luces):
+        super().__init__(modelo,marca,color)
+        self.ruedas = ruedas
+        self.luces = luces
