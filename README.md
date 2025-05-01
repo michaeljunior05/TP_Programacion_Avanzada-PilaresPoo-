7cb8ec650b969880d7e74cd7ada0870574d52704
junior flores <jf6610977@gmail.com>
2025-05-01 16:38:48 -0300

    sobrecargue el metodo mostrar_info

--- README.md
+++ README.md
@@ -20,9 +20,18 @@ Los 4 pilares fundamentales de la #POO son:
 2. ¿Qué pilar permite a las subclases sobrescribir métodos?
 ## Respuesta:
 
+<<<<<<< HEAD
+## Respuesta:
+
+1. El encapsulamiento protege protege los datos dentro de una clase restringiendo su acceso directo, asegurandose que solo metodos especificos puedan modificarlos, mientras que la abstraccion oculta detalles de implementación y expone solo lo esencial para el usuario.
+
+2. 
+
+=======
 1. El encapsulamiento protege protege los datos dentro de una clase restringiendo su acceso directo, asegurandose que solo metodos especificos puedan modificarlos, mientras que la abstraccion oculta detalles de implementación y expone solo lo esencial para el usuario.
 
 2. El pilar que permite a las subclases sobrescribir métodos es la herencia. Esta les permite adquirir los métodos y atributos de una clase padre, pero también les da la capacidad de sobrescribir los métodos heredados para modificar su comportamiento.
+
 
 ## Ejercicio:  
 
--- ejercicio.py
+++ ejercicio.py
@@ -1,19 +1,29 @@
-class Vehículo():
-    def__init__(self,modelo,marca,color):
+class Vehiculo:
+    def __init__(self,modelo:str,marca:str,color:str):
         self.modelo = modelo
         self.marca = marca
         self.color = color
     def mostrar_info(self):
-        return f"Vehículo -- {self.modelo} -- {self.marca} -- {self.color}"
+        return f"Vehículo -- {self.modelo} -- {self.marca} -- {self.color}."
 
 class Moto(Vehiculo):
-    def__init__(self,modelo,marca,color,ruedas,luces):
+    def __init__(self,modelo:str,marca:str,color:int,ruedas:int,luces:int):
         super().__init__(modelo,marca,color)
         self.ruedas = ruedas
         self.luces = luces
+    
+    
+    def mostrar_info(self):
+        return f"Moto -- {super().mostrar_info(self)} -- {self.ruedas} -- {self.luces}."
 
 class Auto(Vehiculo):
-    def__init__(self,modelo,marca,color,ruedas,luces):
+
+    def __init__(self,modelo:str,marca:str,color:str,ruedas:int,luces:int):
         super().__init__(modelo,marca,color)
         self.ruedas = ruedas
         self.luces = luces
+    
+    def mostrar_info(self):
+        return f"Auto -- {super().mostrar_info(self)} -- {self.ruedas} -- {self.luces}."
+
+
