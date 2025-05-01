0945f9af5e34a949625fd9e4d61b1cd94e223ff2
Viviana Enriquez <176007837+DoomMetalLady@users.noreply.github.com>
2025-05-01 15:37:42 -0300

    codigo_con_errores_para_corregir.py

--- codigo_con_errores_para_corregir.py
+++ codigo_con_errores_para_corregir.py
@@ -1,6 +1,6 @@
 class Dog:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name): #corregir asignar el valor al atributo de la instancia
+        self.name = name      #del método __init__, para que name quede almacenado en el objeto.
 
     def speak(self):
         return "woof"
