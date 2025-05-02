cacf0ba6d2b085ef06a3bbdaf092710852eb5e6e
Celina <celinapereyra1@gmail.com>
2025-05-02 14:54:48 -0300

    Update ejercicio.py
    Adicional.

--- ejercicio.py
+++ ejercicio.py
@@ -3,6 +3,25 @@ class Vehiculo:
         self.modelo = modelo
         self.marca = marca
         self.color = color
+        
+    def get_modelo(self):
+        return self._modelo
+        
+    def set_modelo(self, modelo):
+        self._modelo = modelo
+
+    def get_marca(self):
+        return self._marca
+
+    def set_marca(self, marca):
+        self._marca = marca
+
+    def get_color(self):
+        return self._color
+
+    def set_color(self, color):
+        self._color = color
+        
     def mostrar_info(self):
         return f"Vehículo -- {self.modelo} -- {self.marca} -- {self.color}."
 
@@ -11,7 +30,18 @@ class Moto(Vehiculo):
         super().__init__(modelo,marca,color)
         self.ruedas = ruedas
         self.luces = luces
-    
+
+    def get_ruedas(self):
+        return self._ruedas
+        
+    def set_ruedas(self, ruedas):
+        self._ruedas = ruedas
+
+    def get_luces(self):
+        return self._luces
+
+    def set_luces(self, luces):
+        self._luces = luces
     
     def mostrar_info(self):
         return f"Moto -- {super().mostrar_info()} -- ruedas: {self.ruedas} -- luces: {self.luces}."
@@ -22,7 +52,19 @@ class Auto(Vehiculo):
         super().__init__(modelo,marca,color)
         self.ruedas = ruedas
         self.luces = luces
-    
+
+    def get_ruedas(self):
+        return self._ruedas
+
+    def set_ruedas(self, ruedas):
+        self._ruedas = ruedas
+
+    def get_luces(self):
+        return self._luces
+
+    def set_luces(self, luces):
+        self._luces = luces
+        
     def mostrar_info(self):
         return f"Auto -- {super().mostrar_info()} -- ruedas: {self.ruedas} -- luces: {self.luces}."
 
